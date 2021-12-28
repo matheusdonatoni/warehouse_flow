79f65fe7fce52099d6cab98873e922adc26a5e38
Matheus Medeiros Donatoni <matheus.m.donatoni@gmail.com>
2021-12-27 21:11:29 -0300

    runned testes again, added error tests for malformedData and missingEntity

--- test/app/data/usecases/find_all_products_test.dart
+++ test/app/data/usecases/find_all_products_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:warehouse_flow/app/data/local_storage/local_storage_errors.dart';
 import 'package:warehouse_flow/app/data/local_storage/query_helper/query_helper.dart';
 import 'package:warehouse_flow/app/data/usecases/find_all_products_locally.dart';
+import 'package:warehouse_flow/app/domain/helpers/domain_errors.dart';
 
 import '../../infra/database_factory.dart';
 import '../mocks/local_storage_spy.dart';
@@ -28,13 +30,26 @@ void main() {
     );
   });
 
-  test('Should make a successful find query', () async {
-    final future = await sut();
-
-    verify(
-      () => localStorage.find(
-        query: QueryHelper.findAllProducts(),
-      ),
+  test(
+      'Should throw DomainError.malformedData on LocalStorageError.unexpectedFormat',
+      () async {
+    localStorage.mockFindError(
+      LocalStorageError.unexpectedFormat,
     );
+
+    var future = sut();
+
+    expect(future, throwsA(DomainError.malformedData));
+  });
+
+  test('Should throw DomainError.missingEntity on LocalStorageError.notFound',
+      () async {
+    localStorage.mockFindError(
+      LocalStorageError.notFound,
+    );
+
+    var future = sut();
+
+    expect(future, throwsA(DomainError.missingEntity));
   });
 }
