522e4879d0994f7e53d65d05cf798814b66b309d
Matheus Medeiros Donatoni <matheus.m.donatoni@gmail.com>
2021-12-29 19:19:27 -0300

    added the setup method

---
+++ test/app/infra/mocks/database_spy.dart
@@ -0,0 +1,6 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseSpy extends Mock implements Database {
+  
+}
--- test/app/infra/sql/sqlite_adpter_test.dart
+++ test/app/infra/sql/sqlite_adpter_test.dart
@@ -1,3 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:warehouse_flow/app/infra/sql/sqlite_adpter.dart';
+
+import '../mocks/database_spy.dart';
+
 void main() {
-  
+  late SQLiteAdapter sut;
+  late DatabaseSpy database;
+
+  setUp(() {
+    database = DatabaseSpy();
+    sut = SQLiteAdapter(database);
+  });
 }
