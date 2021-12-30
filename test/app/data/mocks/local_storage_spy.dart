938eee638918f609ee9723f302c342f604ca0e99
Matheus Medeiros Donatoni <matheus.m.donatoni@gmail.com>
2021-12-29 22:04:21 -0300

    added mockRawQueryCall

--- test/app/data/mocks/local_storage_spy.dart
+++ test/app/data/mocks/local_storage_spy.dart
@@ -3,13 +3,10 @@ import 'package:warehouse_flow/app/data/local_storage/local_storage.dart';
 import 'package:warehouse_flow/app/data/local_storage/local_storage_errors.dart';
 
 class LocalStorageSpy extends Mock implements LocalStorage {
-  When _mockFindCall() => when(() => find(
-        query: any(named: 'query'),
-      ));
-  void mockFind(dynamic result) =>
-      this._mockFindCall().thenAnswer(
-            (_) async => result,
-          );
+  When _mockFindCall() => when(() => find(query: any(named: 'query')));
+  void mockFind(dynamic result) => this._mockFindCall().thenAnswer(
+        (_) async => result,
+      );
   void mockFindError(LocalStorageError error) => this._mockFindCall().thenThrow(
         error,
       );
--- test/app/infra/mocks/database_spy.dart
+++ test/app/infra/mocks/database_spy.dart
@@ -2,5 +2,5 @@ import 'package:mocktail/mocktail.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseSpy extends Mock implements Database {
-  
+  When mockRawQueryCall() => when(() => this.rawQuery(any(), [any()]));
 }
