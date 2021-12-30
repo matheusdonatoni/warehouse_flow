7d167cdae7ea8a801496c936061aed72a210326d
Matheus Medeiros Donatoni <matheus.m.donatoni@gmail.com>
2021-12-29 22:37:11 -0300

    basic DatabaseSpy mock functions built

--- test/app/infra/mocks/database_spy.dart
+++ test/app/infra/mocks/database_spy.dart
@@ -2,5 +2,29 @@ import 'package:mocktail/mocktail.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseSpy extends Mock implements Database {
-  When mockRawQueryCall() => when(() => this.rawQuery(any(), [any()]));
+  When _mockRawQueryCall() => when(() => this.rawQuery(any(), [any()]));
+  void mockRawQuery(String sql, [List<Object?>? arguments]) =>
+      this._mockRawQueryCall().thenAnswer((_) async => [
+            {'json': '{"id": 1, "anyKey": "anyValue"}'}
+          ]);
+  void mockRawQueryError(DatabaseException exception) =>
+      this._mockRawQueryCall().thenThrow(exception);
+
+  When _mockRawDeleteCall() => when(() => this.rawDelete(any(), [any()]));
+  void mockRawDelete(String sql, [List<Object?>? arguments]) =>
+      this._mockRawDeleteCall().thenAnswer((_) async => 1);
+  void mockRawDeleteError(DatabaseException exception) =>
+      this._mockRawDeleteCall().thenThrow(exception);
+
+  When _mockRawUpdateCall() => when(() => this.rawUpdate(any(), [any()]));
+  void mockRawUpdate(String sql, [List<Object?>? arguments]) =>
+      this._mockRawUpdateCall().thenAnswer((_) async => 1);
+  void mockRawUpdateError(DatabaseException exception) =>
+      this._mockRawUpdateCall().thenThrow(exception);
+
+  When _mockRawInsertCall() => when(() => this.rawInsert(any(), [any()]));
+  void mockRawInsert(String sql, [List<Object?>? arguments]) =>
+      this._mockRawInsertCall().thenAnswer((_) async => 1);
+  void mockRawInsertError(DatabaseException exception) =>
+      this._mockRawInsertCall().thenThrow(exception);
 }
