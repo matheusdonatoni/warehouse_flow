42116392ddaf200ff3eafd72ef5133b70dc1dae1
Matheus Medeiros Donatoni <matheus.m.donatoni@gmail.com>
2022-01-04 19:16:45 -0300

    write first verify test

--- test/app/data/usecases/find_warehouse_operations_locally_test.dart
+++ test/app/data/usecases/find_warehouse_operations_locally_test.dart
@@ -1,20 +1,34 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:warehouse_flow/app/data/local_storage/query_helper/query_helper.dart';
 import 'package:warehouse_flow/app/data/usecases/find_warehouse_operations_locally.dart';
+import 'package:warehouse_flow/app/domain/usecases/find_warehouse_operations.dart';
 
 import '../mocks/local_storage_factory.dart';
 import '../mocks/local_storage_spy.dart';
 
 void main() {
+  late FindWarehouseOperationsParams params;
+  late List<Map<String, dynamic>> databaseResult;
   late FindWarehouseOperationsLocally sut;
   late LocalStorageSpy localStorage;
-  late FindWarehouseOperationsLocallyParams params;
-  late List<Map<String, dynamic>> databaseResult;
 
   setUp(() {
-    params = FindWarehouseOperationsLocallyParams(warehouseId: 1);
+    params = FindWarehouseOperationsParams(id: 1);
     databaseResult = LocalStorageFactory.makeWarehouseOperationListResult();
     localStorage = LocalStorageSpy();
     localStorage.mockFind(databaseResult);
     sut = FindWarehouseOperationsLocally(localStorage: localStorage);
+  });
+
+  test('Should call LocalStorage.find with correct values', () async {
+    await sut(params);
+
+    verify(
+      () => localStorage.find(
+        query: QueryHelper.findWarehouseOperations,
+        arguments: [1],
+      ),
+    );
   });
 }
