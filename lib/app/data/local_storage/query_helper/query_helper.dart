2bc3d986162722acc0329f837621eb9330919e11
Matheus Medeiros Donatoni <matheus.m.donatoni@gmail.com>
2022-01-04 18:10:04 -0300

    makeAllWarehouseOperationListResult LocalStorage Factory

--- lib/app/data/local_storage/query_helper/query_helper.dart
+++ lib/app/data/local_storage/query_helper/query_helper.dart
@@ -1,5 +1,6 @@
 class QueryHelper {
-  static String get findWarehouseWithRegister => '''SELECT
+  static String get findWarehouseWithRegister {
+    return '''SELECT
     json_object(
       'id', id, 
       'name', name,
@@ -68,8 +69,10 @@ class QueryHelper {
         ) FROM registers WHERE registers.warehouseId = warehouses.id
       ) 
     ) AS json FROM warehouses WHERE warehouses.id = ?''';
+  }
 
-  static String get findWarehouseOperations => '''SELECT
+  static String get findWarehouseOperations {
+    return '''SELECT
     SELECT json_group_array(
       json_object(
         'id', operations.id, 
@@ -138,8 +141,10 @@ class QueryHelper {
     JOIN warehouses
     on warehouses.id = spots.warehouseid
     WHERE warehouses.id = ?''';
+  }
 
-  static String get findAllProducts => '''SELECT
+  static String get findAllProducts {
+    return '''SELECT
    json_group_array(
      json_object(
         'id', id, 
@@ -151,87 +156,12 @@ class QueryHelper {
         'updatedAt', updatedat
       )
     ) as json FROM products''';
+  }
 
-  static String get insertIntoOperations => '''INSERT 
+  static String get insertIntoOperations {
+    return '''INSERT 
   INTO operations 
-  (amount, type, registerid, productid, spotId, addressId, positionId, createdAt, updatedAt) 
-  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''';
+  (amount, type, productid, positionId, createdAt, updatedAt) 
+  VALUES (?, ?, ?, ?, ?, ?)''';
+  }
 }
-
-// Aliased columns query for easier joined table operations.
-
-  // final String _warehouseQuery = '''warehouses.id as warehouse_id,
-  // warehouses.name as warehouse_name,
-  // warehouses.createdat as warehouse_createdAt,
-  // warehouses.updatedat as warehouse_updatedAt''';
-
-  // final String _spotQuery = '''spots.id as spot_id,
-  // spots.name as spot_name,
-  // spots.createdat as spot_createdAt,
-  // spots.updatedat as spot_updatedAt''';
-
-  // final String _addressQuery = '''addresses.id as address_id,
-  // addresses.name as address_name,
-  // addresses.indicator as address_indicator,
-  // addresses.createdat as address_createdAt,
-  // addresses.updatedat as address_updatedAt''';
-
-  // final String _positionQuery = '''positions.id as position_id,
-  // positions.height as position_height,
-  // positions.depth as position_depth,
-  // positions.type as position_type,
-  // positions.createdat as position_createdAt,
-  // positions.updatedat as position_updatedAt''';
-
-  // final String _registerQuery = '''registers.id as register_id,
-  // registers.warehouseId as register_warehouseId,
-  // registers.createdat as register_createdAt,
-  // registers.updatedat as register_updatedAt''';
-
-  // final String _operationQuery = '''operations.id as operation_id,
-  // operations.amount as operation_amount,
-  // operations.type as operation_type,
-  // operations.spotId as operation_spotId,
-  // operations.addressId as operation_addressId,
-  // operations.positionId as operation_positionId,
-  // operations.registerId as operation_registerId,
-  // operations.productId as operation_productId,
-  // operations.createdat as operation_createdAt,
-  // operations.updatedat as operation_updatedAt''';
-
-  // final String _productQuery = '''products.id as product_id,
-  // products.code as product_code,
-  // products.description as product_description,
-  // products.unit as product_unit,
-  // products.type as product_type,
-  // products.createdat as product_createdAt,
-  // products.updatedat as product_updatedAt''';
-
-  // static String findWarehouseWithResiter(int id) {
-  //   final helper = QueryHelper();
-
-  //   return '''SELECT
-  //   ${helper._warehouseQuery},
-  //   ${helper._registerQuery},
-  //   ${helper._spotQuery},
-  //   ${helper._spotQuery},
-  //   ${helper._addressQuery},
-  //   ${helper._positionQuery},
-  //   ${helper._operationQuery},
-  //   ${helper._productQuery}
-  //   FROM warehouses
-  //   LEFT JOIN registers
-  //   ON warehouses.id = registers.warehouseId
-  //   LEFT JOIN operations
-  //   ON registers.id = operations.registerId
-  //   LEFT JOIN spots
-  //   ON spots.id = operations.spotId
-  //   LEFT JOIN addresses
-  //   ON addresses.id = operations.addressId
-  //   LEFT JOIN positions
-  //   ON positions.id = operations.positionId
-  //   LEFT JOIN products
-  //   ON products.id = operations.productId
-  //   WHERE registers.warehouseId = $id
-  //   ORDER BY operations.createdAt DESC''';
-  // }
--- test/app/data/mocks/local_storage_factory.dart
+++ test/app/data/mocks/local_storage_factory.dart
@@ -50,6 +50,85 @@ class LocalStorageFactory {
         }
       };
 
+  static List<Map<String, dynamic>> makeAllWarehouseOperationListResult() => [
+        {
+          "id": 1,
+          "amount": 73.0,
+          "type": "teste",
+          "createdAt": "2022-01-03T20:05:04.586",
+          "updatedAt": "2022-01-03T20:05:04.586",
+          "product": {
+            "id": 500,
+            "code": 611047,
+            "description":
+                "BROCA HELIC;ACO RAP;5/64POL;50MM;HASTE CIL;ANSI B94 11M",
+            "unit": "UN",
+            "type": "APL",
+            "createdAt": "2021-11-23T18:32:20.268",
+            "updatedAt": "2021-11-23T18:32:20.268"
+          },
+          "position": {
+            "id": 23,
+            "height": 1,
+            "depth": 3,
+            "type": "Embalagens",
+            "createdAt": "2021-11-14T01:07:29.812",
+            "updatedAt": "2021-11-14T01:07:29.812"
+          },
+          "address": {
+            "id": 3,
+            "name": "Rua A",
+            "indicator": "3",
+            "createdAt": "2021-11-14T01:07:29.812",
+            "updatedAt": "2021-11-14T01:07:29.812"
+          },
+          "spot": {
+            "id": 5,
+            "name": "Câmara 5",
+            "createdAt": "2021-12-07T18:58:05.326",
+            "updatedAt": "2021-12-07T18:58:05.326"
+          }
+        },
+        {
+          "id": 2,
+          "amount": 25.0,
+          "type": "teste2",
+          "createdAt": "2022-01-03T20:05:04.586",
+          "updatedAt": "2022-01-03T20:05:04.586",
+          "product": {
+            "id": 11,
+            "code": 34621,
+            "description":
+                "IINVERSOR FREQ;TRIF;380/440V;5CV;WEG/CFW080100T3848PSZ",
+            "unit": "UN",
+            "type": "APL",
+            "createdAt": "2021-11-23T18:32:20.268",
+            "updatedAt": "2021-11-23T18:32:20.268"
+          },
+          "position": {
+            "id": 44,
+            "height": 2,
+            "depth": 1,
+            "type": "Embalagens",
+            "createdAt": "2021-11-14T01:07:29.812",
+            "updatedAt": "2021-11-14T01:07:29.812"
+          },
+          "address": {
+            "id": 4,
+            "name": "Rua A",
+            "indicator": "4",
+            "createdAt": "2021-11-14T01:07:29.812",
+            "updatedAt": "2021-11-14T01:07:29.812"
+          },
+          "spot": {
+            "id": 5,
+            "name": "Câmara 5",
+            "createdAt": "2021-12-07T18:58:05.326",
+            "updatedAt": "2021-12-07T18:58:05.326"
+          }
+        }
+      ];
+
   static List<Map<String, dynamic>> makeAllProductsListResult() => [
         {
           "id": 1,
